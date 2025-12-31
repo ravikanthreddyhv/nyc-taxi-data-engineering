6af84cadf255bbd478f892427e3870375342be51
rhalaharvivi <ravikanth@ravis-MacBook-Air.local>
2025-12-31 13:59:09 -0800

    chore: terraform fmt

--- day5-infra/terraform/envs/dev/variables.tf
+++ day5-infra/terraform/envs/dev/variables.tf
@@ -1,10 +1,10 @@
-variable "aws_region"      { type = string }
-variable "s3_bucket_name"  { type = string }
-variable "lambda_name"     { type = string }
+variable "aws_region" { type = string }
+variable "s3_bucket_name" { type = string }
+variable "lambda_name" { type = string }
 variable "lambda_zip_path" { type = string }
 
-variable "db_identifier"   { type = string }
-variable "db_name"         { type = string }
+variable "db_identifier" { type = string }
+variable "db_name" { type = string }
 variable "master_username" { type = string }
 variable "master_password" {
   type      = string
@@ -12,6 +12,6 @@ variable "master_password" {
 }
 
 
-variable "vpc_id"          { type = string }
-variable "subnet_ids"      { type = list(string) }
-variable "allowed_cidrs"   { type = list(string) }
+variable "vpc_id" { type = string }
+variable "subnet_ids" { type = list(string) }
+variable "allowed_cidrs" { type = list(string) }
